f1fb41a285c8e14454df7f2e05b7d104a8419a95
kvo <karl-lost@gmx.de>
2022-05-13 16:13:28 +0200

    HtmlParser angefangen

--- classes/HtmlParser.js
+++ classes/HtmlParser.js
@@ -3,4 +3,7 @@ class HtmlParser{
     constructor(entities){
         this.entities = entities;
     }
+    readPath = `./templates/template_html.html`;
+    writePath = `./out/${utilities.setDate()}/file.html`;
+
 }
