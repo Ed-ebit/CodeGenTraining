efa2410e783d45e5c5c5ad7f7f3b95bd319cf695
kvo <karl-lost@gmx.de>
2022-05-18 13:22:00 +0200

    fast fertig. kann returnstring noch net schreiben

--- classes/Render.js
+++ classes/Render.js
@@ -2,33 +2,33 @@ class Render {
 
     /**
      *
-     * @param {array} entities
+     * @param {object} entities
      * @param {string} template
      */
     constructor(entities, template ){
-        // console.log(entities, template);
         this.entities = entities;
         this.template = template;
-        // this.generate = generate;
 
     }
 
     generate(){
-        const partial = this.findByMarker("CUSTOMER");
-        let returnStr = '';
+        let partial = this.findByMarker("CUSTOMER");
 
+        let returnStr = '';
         this.entities.forEach(entity => {
-            Object.keys(entity).forEach(property => {
-                console.log(entity, entity[property])
-                returnStr += `${ partial.replace(`##${property}##`, entity[property])}\n`;
+            let templateToReplace = partial
+            Object.keys(entity).forEach(key => {
+               templateToReplace = templateToReplace.replace(`##${key}##`, entity[key])
             })
+            returnStr += templateToReplace;
         });
+
+         console.log(returnStr)
 
         return this.template.replace(`##CUSTOMER_START##.*##CUSTOMER_END##`, returnStr);
     }
 
     findByMarker(marker){
-        // console.log(this.template.split(`##${marker}_START##`)[1].split(`##${marker}_END##`)[0])
         return this.template.split(`##${marker}_START##`)[1].split(`##${marker}_END##`)[0];
     }
 
