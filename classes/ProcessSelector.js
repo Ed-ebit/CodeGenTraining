e1161ecb06580a05218dd2d4bdb0e388151371b3
kvo <karl-lost@gmx.de>
2022-05-24 13:43:46 +0200

    refactor des Process Selector abgeschlossen

--- classes/ProcessSelector.js
+++ classes/ProcessSelector.js
@@ -2,20 +2,36 @@ const fs = require("fs");
 
 class ProcessSelector {
 
+    static checkPossibleConfigs() {
+        let availableConfigs = fs.readdirSync(`./Locator/`)
+        let configList = ''
+        // for (let i=0; i<availableConfigs.length; i++)
+        // {
+        //     configList += availableConfigs[i].split('Config.js') + ' '
+        // }
+        availableConfigs.forEach((config) => {
+            configList += '\n' + config.split('Config.js')[0]
+        })
+        return configList;
+    }
+
     static chooseProcess(customValue) {
+        let possibleConfigs = ProcessSelector.checkPossibleConfigs();
         try {
             customValue = customValue.charAt(0).toUpperCase() + customValue.slice(1);
             if (fs.existsSync(`./Locator/${customValue}Config.js`)) {
                 console.log(`${customValue} wird generiert`);
                 const config = require(`../Locator/${customValue}Config`);
                 return config.config;
-            } else {
-                console.log('Bitte bekanntes Argument("xml", "html" oder "csv") beim Start angeben!');
+            }
+            else {
+                console.log(`Bitte bekanntes Argument beim Start angeben!\nbekannte Argumente: ${possibleConfigs} `);
                 process.exit();
             }
+
         } catch {
             console.log('Zusätzliches Argument nicht erkannt/nicht vorhanden. \n' +
-                'Bitte "xml", "html" oder "csv" als Argument beim Start angeben!')
+                'Bitte ' + possibleConfigs + '\nals Argument beim Start angeben!')
             process.exit();
         }
 
